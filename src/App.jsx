eb35e46e42ad133cce39f10a314035f9e52ffaa5
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-08-04 16:41:31 +0300

    panel setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from "react"
+import Home from "./components/Panel"
+import About from "./components/About"
+import { Routes, Route } from "react-router-dom"
 
 import "./App.css"
 
 function App() {
-  return <>
-  </>
+  return (
+    <>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Panel.jsx
@@ -0,0 +1,12 @@
+const Panel = () => {
+return <>
+<div class="container">
+  <div class="container-item">Info</div>
+  <div class="container-item">Add</div>
+  <div class="container-item">Edit</div>
+  <div class="container-item">Delete</div>
+
+</div>
+</>
+}
+export default Panel
