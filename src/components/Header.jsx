67c5c43aab1922b09d2c05f57c6501c81dac8ad8
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-08-05 16:35:29 +0300

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,28 @@
 import { NavLink } from "react-router-dom"
+import houseImg from "../assets/house.png"
+import aboutImg from "../assets/about.png"
+import searchImg from "../assets/search.png"
 
-const Header= () => {
-return(
-  <div>
-  <header>
-    <h1>LostParadise</h1>
-  
-  <nav>
-  <ul>
-    <li><NavLink to="/">Home</NavLink></li>
-    <li><NavLink to="/about">About</NavLink></li>
-  </ul>
-</nav>
-
-   
-
-  </header>
-  
- </div>
-)
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <nav>
+          <NavLink to="/games">
+            <div className="iconContainer">
+              <img src={houseImg} height="12rem" />
+            </div>
+          </NavLink>
+          <NavLink to="/about">
+            <div className="iconContainer">
+              <img src={aboutImg} height="12rem" />
+            </div>
+          </NavLink>
+        </nav>
+        <div className="search"><img src={searchImg} height="12rem" className="searchIcon"/></div>
+      </div>
+    </>
+  )
 }
 
-export default Header 
+export default Header
