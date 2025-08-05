82c4a9e5cfe92502a63c62a8c5dc128b2b6df48d
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-08-05 18:23:57 +0300

    debugging issue of posting

--- src/services/GameForm.js
+++ src/services/GameForm.js
@@ -1,10 +1,11 @@
-import Client from "./API";
-export const newRecored=async(payload)=>{
-try {
-    // const res = await Client.post('/games/new', payload)
+import Client from "./API"
+
+export const newRecored = async (payload) => {
+  try {
+ //   const res = await Client.post("/games/new", payload)
     console.log(payload)
-    // return res.data
-} catch (error) {
+  //  return res.data
+  } catch (error) {
     console.log(error)
-}
+  }
 }
